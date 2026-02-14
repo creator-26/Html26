f2848ee52311b57159768949f03ff835b0d1befd
creator-26 <pholalexanderwuamputsaggamboa@gmail.com>
2026-02-13 21:30:38 -0500

    Add button click event to change title color

--- proyecto1/script.js
+++ proyecto1/script.js
@@ -1,1 +1,9 @@
+const boton = document.getElementById("boton");
+const titulo = document.getElementById("titulo");
+const texto = document.getElementById("texto");
 
+boton.addEventListener("click", function () {
+    titulo.style.color = "yellow";
+    texto.textContent = "✅ JavaScript está funcionando correctamente!";
+    console.log("El botón fue presionado");
+});
